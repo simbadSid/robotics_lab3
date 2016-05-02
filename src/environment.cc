4626f79af491629f8755d72fa7e559cdcafca3d0
simbadSid <littlegirle@localhost.localdomain>
2016-05-02 16:38:07 +0200

    for eline

---
+++ src/environment.cc
@@ -0,0 +1,16 @@
+/*
+ * environment.cc
+ *
+ *  Created on: May 2, 2016
+ *      Author: littlegirle
+ */
+
+#include "environment.h"
+
+
+
+
+Environment::Environment()
+{
+	this->matrix = malloc (3 * sizeof(int));
+}
---
+++ src/environment.h
@@ -0,0 +1,36 @@
+/*
+ * environment.h
+ *
+ *  Created on: May 2, 2016
+ *      Author: littlegirle
+ */
+
+#ifndef ENVIRONMENT_H_
+#define ENVIRONMENT_H_
+
+#include <stdlib.h>
+
+
+class Environment
+{
+// -----------------------------
+// Attributes
+// -----------------------------
+	private:
+		int** matrix;
+
+
+// -----------------------------
+// Builder
+// -----------------------------
+	public:
+		Environment();
+		~Environment();
+
+// -----------------------------
+// Local methods
+// -----------------------------
+
+}
+
+#endif /* ENVIRONMENT_H_ */
